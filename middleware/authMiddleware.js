050e9bb1fd7c8cf0778089cdd3955a94aabbad33
Ragul Seeman <seeman.ragul@divum.in>
2024-06-05 15:26:53 +0530

    Code cleanup

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,15 +4,15 @@ module.exports = function(req, res, next) {
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
-  console.log('Token received:', token); // Add this line to log the token
+  console.log('Token received:', token); 
 
   try {
     const decoded = jwt.verify(token, 'F66DB4EC116F3');
-    console.log('Decoded token:', decoded); // Add this line to log the decoded token
+    console.log('Decoded token:', decoded);
     req.admin = decoded.admin;
     next();
   } catch (err) {
-    console.error('Token verification failed:', err); // Add this line to log any error
+    console.error('Token verification failed:', err); 
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
--- middleware/upload.js
+++ middleware/upload.js
@@ -1,30 +1,30 @@
 const multer = require('multer');
 const path = require('path');
 
-// Set storage engine
+
 const storage = multer.diskStorage({
-  destination: './uploads/', // Specify the folder to store uploaded files
+  destination: './uploads/', 
   filename: function(req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
 
-// Init upload
+
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1000000 }, // 1MB file size limit
+  limits: { fileSize: 1000000 }, 
   fileFilter: function(req, file, cb) {
     checkFileType(file, cb);
   }
 });
 
-// Check file type
+
 function checkFileType(file, cb) {
-  // Allowed ext
+ 
   const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
+ 
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
+
   const mimetype = filetypes.test(file.mimetype);
 
   if (mimetype && extname) {
